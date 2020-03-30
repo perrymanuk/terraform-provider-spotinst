c03649f0286c41951bc8e438b881004311cb6d25
ebarrios <esteban.barrios@trivago.com>
2020-03-30 13:01:32 +0200

    Added const to const.go for projectId

--- spotinst/elastigroup_gcp_network_interface/consts.go
+++ spotinst/elastigroup_gcp_network_interface/consts.go
@@ -5,6 +5,7 @@ import "github.com/terraform-providers/terraform-provider-spotinst/spotinst/comm
 const (
 	NetworkInterface commons.FieldName = "network_interface"
 	Network          commons.FieldName = "network"
+	ProjectId        commons.FieldName = "projectId" //ESTE
 	AccessConfigs    commons.FieldName = "access_configs"
 	AliasIPRanges    commons.FieldName = "alias_ip_ranges"
 
